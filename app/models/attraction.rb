61fa11dab8c2926cd1c61b332f1423acfef0cadc
liquidsword <audenrobertson@gmail.com>
2020-02-02 23:00:52 -0500

    initial commit - add user model location model

---
+++ app/models/attraction.rb
@@ -0,0 +1,4 @@
+class Attraction < ApplicationRecord
+  belongs_to :location
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :attractions
+  resources :locations
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :locations
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    end
+  end
+end
