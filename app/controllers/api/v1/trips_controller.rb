66f13751c6c74511fee30370a8105d14c373891d
liquidsword <audenrobertson@gmail.com>
2020-02-18 20:45:59 -0500

    created trips controller
    created the trips controller

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,2 +1,49 @@
 class Api::V1::TripsController < ApplicationController
+  def index
+    if logged_in?
+      @trips = current_user.trips
+
+      render json: @trips
+    else
+      render json: {
+        error: "You aren't logged in!!!"
+      }
+    end
+  end
+
+  def show
+    render json: @trip
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+      if @trip.save
+        render json: @trip, status: :created, location: @trip
+      else
+        render json: @trip.errors, status: :unprocessable_entity
+      end
+  end
+
+  def update
+    if @trip.update(trip_params)
+      render json: @trip
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @trip.destroy
+  end
+
+  private
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:start_date, :end_date, :user_id, :location_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users
       resources :locations
       resources :visits
       resources :trips
       resources :comments
       resources :attractions
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      resources :users
     end
   end
 end
