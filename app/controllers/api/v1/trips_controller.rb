0f453a6c82c601c02a2ab45b5317a6441144310a
liquidsword <audenrobertson@gmail.com>
2020-03-19 21:55:05 -0400

    updated trip controller
    added logic to the update, create and destroy to flow with the front end

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::TripsController < ApplicationController
   end
 
   def create
-    @trip = Trip.new(trip_params)
+    @trip = current_user.trips.build(trip_params)
       if @trip.save
         render json: TripSerializer.new(@trip), status: :created
       else
@@ -31,14 +31,24 @@ class Api::V1::TripsController < ApplicationController
 
   def update
     if @trip.update(trip_params)
-      render json: @trip
+      render json: TripSerializer.new(@trip), status: :ok
     else
-      render json: @trip.errors, status: :unprocessable_entity
+      error_resp = {
+        error: @trip.errors.full_messages.to_sentence
+      }
+      render json: error_resp, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @trip.destroy
+    if @trip.destroy
+      render json: { data: "Trip destroyed!!" }, status: :ok
+    else
+      error_resp = {
+        error: "Trip not found!!!"
+      }
+      render json: error_resp, status: :unprocessable_entity
+    end
   end
 
   private
@@ -48,7 +58,7 @@ class Api::V1::TripsController < ApplicationController
   end
 
   def trip_params
-    params.require(:trip).permit(:start_date, :end_date, :user_id, :name)
+    params.require(:trip).permit(:start_date, :end_date, :name)
   end
 
 end
